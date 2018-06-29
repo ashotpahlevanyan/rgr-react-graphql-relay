509b6f0def4e0b4c57e3ab1101f3d64904493a83
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-06-29 19:30:56 +0400

    added webpack config and used react in app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,11 @@
-var Hello = React.createClass({
-	render: function() {
-		return React.createElement("h3", null, "Hello React!");
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+
+class Hello extends React.Component {
+	render() {
+			return React.createElement("h3", null, "Hello React!");
 	}
-});
+}
 
 ReactDOM.render(React.createElement(Hello), document.getElementById('react'));
