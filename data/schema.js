33e518392c2cdad9a28828f956c81ed78ccd7983
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-06-30 18:24:26 +0400

    added newly defined  counterType

--- data/schema.js
+++ data/schema.js
@@ -2,22 +2,36 @@ import {
 	GraphQLSchema,
 	GraphQLObjectType,
 	GraphQLInt,
-	GraphQLString
+	GraphQLString,
+	GraphQLList
 } from 'graphql';
 
-let counter = 42;
+let data = [
+	{
+		counter: 42
+	},
+	{
+		counter: 43
+	},
+	{
+		counter: 44
+	}
+];
+
+let counterType = new GraphQLObjectType({
+	name: 'Counter',
+	fields: () => ({
+		counter: { type: GraphQLInt }
+	})
+});
 
 let schema = new GraphQLSchema({
 	query: new GraphQLObjectType({
 		name: 'Query',
 		fields: () => ({
-			counter: {
-				type: GraphQLInt,
-				resolve: () => counter
-			},
-			message: {
-				type: GraphQLString,
-				resolve: () => 'Hello GraphQL'
+			data: {
+				type: new GraphQLList(counterType),
+				resolve: () => data
 			}
 		})
 	}),
